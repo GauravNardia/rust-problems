92ae4c8477237a2afd7832f65e55a1d6e2baef1c
Gaurav Nardia <codegaurav07@gmail.com>
2026-02-11 17:41:54 +0530

    add options and result

---
+++ src/option.rs
@@ -0,0 +1,21 @@
+
+
+//  option retuns some value of NONE.
+
+fn main() {
+    let index = find_first_a(String::from("praeet"));
+
+    match index {
+        Some(value) => print!("{}", value),
+        None  => println!("a not fpund"),
+    }
+} 
+
+fn find_first_a(s: String) -> Option<i32> {
+    for (index, char) in s.chars().enumerate() {
+        if char == 'a' {
+            return Some(index as i32)
+        }
+    }
+    return None
+}
---
+++ src/result.rs
@@ -0,0 +1,3 @@
+// result enum return OK or Err value.
+//  we use in error handling
+
